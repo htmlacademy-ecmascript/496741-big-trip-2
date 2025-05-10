a006f130887ad9ff2f2774660134b23a86e0be90
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-05-10 19:49:06 +0400

    Исправляет вывод разницы во времени

--- src/utils/trip.js
+++ src/utils/trip.js
@@ -5,11 +5,21 @@ const humanizeDate = (date, dateFormat) => date ? dayjs(date).format(dateFormat)
 const findTimeInterval = (dateFrom, dateTo) => {
   const date1 = dayjs(dateFrom);
   const date2 = dayjs(dateTo);
+  let diff = '';
 
+  const diffDays = date2.diff(date1, 'day');//в днях
   const diffHours = date2.diff(date1, 'hour'); // Разница в часах
   const diffMinutes = date2.diff(date1, 'minute') % 60; // Оставшиеся минуты
 
-  return diffHours ? `${diffHours}H ${diffMinutes}M` : `${diffMinutes}M`;
+  if (diffDays) {
+    diff = `${diffDays}D ${diffHours}H ${diffMinutes}M`;
+  } else if (diffHours) {
+    diff = `${diffHours}H ${diffMinutes}M`;
+  } else {
+    diff = `${diffMinutes}M`;
+  }
+
+  return diff;
 };
 
 function isFutureDate(targetDate) {
