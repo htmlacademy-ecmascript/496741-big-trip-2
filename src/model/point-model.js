2816124d86645a1b0857e7912998921e5c03a65b
Vagiza Svidchenkova <w.svidchenkova@gmail.com>
2025-05-10 20:21:23 +0400

    Устанавливает пустую дату в новой точке

--- src/model/point-model.js
+++ src/model/point-model.js
@@ -3,8 +3,8 @@ import { WaypointType } from '../const';
 export default function createNewPoint() {
   return {
     basePrice: 0,
-    dateFrom: '1970-01-01T00:00:00.000Z',
-    dateTo: '1970-01-01T00:00:00.000Z',
+    dateFrom: '',
+    dateTo: '',
     destination: null,
     isFavorite: false,
     offers: [],
